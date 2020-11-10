1ecdcefd1989c0243a6ac06c665b7f6f5b0d06d6
WUYAOQING <yaoqingwu@foxmail.com>
2020-11-10 15:28:39 +0800

    增加配置文件

--- Learn/5VueCli/myproc/src/main.js
+++ Learn/5VueCli/myproc/src/main.js
@@ -8,3 +8,5 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+console.log(process.env.VUE_APP_URL);
--- Learn/5VueCli/myproc/src/router/index.js
+++ Learn/5VueCli/myproc/src/router/index.js
@@ -13,17 +13,20 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    //懒加载路由
+    component: () => import('../views/About.vue')
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
-
+/*
+	histroy:
+	localhost:8080/user/list
+	hash:
+	localhost:8080/#/user/list
+*/
 export default router
