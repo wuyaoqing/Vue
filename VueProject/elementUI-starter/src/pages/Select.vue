f127e2dbb90ad69c0f56c5cee666f69f14273c27
wuyaoqing <wuyaoqing@allwinnertech.com>
2022-04-01 15:14:03 +0800

    测试

--- VueProject/elementUI-starter/src/pages/Select.vue
+++ VueProject/elementUI-starter/src/pages/Select.vue
@@ -9,25 +9,13 @@
 	export default {
 		data() {
 			return {
-				options: [{
-					value: '选项1',
-					label: '黄金糕测测二测错测错错错测二'
-				}, {
-					value: '选项2',
-					label: '双皮奶'
-				}, {
-					value: '选项3',
-					label: '蚵仔煎'
-				}, {
-					value: '选项4',
-					label: '龙须面'
-				}, {
-					value: '选项5',
-					label: '北京烤鸭'
-				}],
+				options: [],
 				value5: [],
 				value11: []
 			}
+		},
+		created(){
+			this.options.push({label:'a',value:'a'})
 		}
 	}
 </script>
